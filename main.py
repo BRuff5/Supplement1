9ef57154ad040a590e78bed346e0c5e1942bfbd3
BLR <bruff@Macky-Max.local>
2024-11-21 19:15:56 -0500

    add featue

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 def add(num1, num2):
-    return num1 + num2
+    """Adds two numbers and returns the result.
+    
+    Args:
+        num1: the first summand.
+        num2: the second summand.
+    
+    Returns:
+        The sum of the two numbers   
+        return num1 + num2
+    """
+def count(string):
+    return len(string)
 
 def test_should_return_four_for_two_and_two():
-    assert_add(2, 2) == 4
+    assert add(2, 2) == 4
     
+def test_should_return_length_five_for_hello():
+    assert count("hello") == 5
